21bb3041489f573b77a3da23471102045ed4b8a6
melihozsecgin <turkizozsecgin@gmail.com>
2020-06-17 11:09:19 +0300

    tasarim degistirildi- bootstrap eklendi

--- assets/js/titleManager.js
+++ assets/js/titleManager.js
@@ -9,10 +9,26 @@
 
 // Set the default title
 function resetTitle() {
-  document.title = 'Home P2age';
+  document.title = 'Home Page';
 }
 
 // Set the title
 function setTitle(title) {
   document.title = title;
 }
+
+function deleteTitle() {
+  document.title = "";
+}
+
+$(document).ready(function() {
+  $('.setTitle').click(function() {
+    var setTitle = prompt('Set the title');
+    if(setTitle == "") {
+      alert('Please enter a title!');
+    }
+    else {
+      console.log(setTitle);
+    }
+  })
+});
