d1ea6f627dac1f9e94ec896a8eac517fc4903e05
melihozsecgin <turkizozsecgin@gmail.com>
2020-06-18 16:32:53 +0300

    counter degerleri degistirildi

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,20 +1,51 @@
 $(function(){
 
-    var seconds = 11160;
+    var seconds = 14400;
     var counterDiv = $("div.counter span");
 
     $.startCounter = function(){
         if(seconds > 1){
+            minute = 14400 / 60;
             seconds--;
             counterDiv.text(seconds);
             console.log(seconds);
             setTitle(seconds + ' seconds left - Home Page');
         }
         else {
-          resetTitle();
+            resetTitle();
         }
     }
     counterDiv.text(seconds);
     setInterval("$.startCounter()", 1000);
 
 });
+
+function startTimer(duration, display) {
+    var timer = duration, hours, minutes, seconds;
+    setInterval(function () {
+        hours = parseInt((timer / 60)/60, 10);
+        minutes = Math.floor((timer/60) % 60);
+        seconds = parseInt(timer % 60, 10);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = hours + ":" + minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 180,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+
+
+$(document).ready(function() {
+    $("#brand_icon").html();
+})
